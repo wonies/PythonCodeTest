83145b9da206d0b4bd7d4074ac0e9b7de9350dfd
WONIE <wonie@WONIEui-MacBookPro.local>
2024-10-01 22:23:21 +0900

    section

--- section4/사과나무_다이아몬드.py
+++ section4/사과나무_다이아몬드.py
@@ -12,7 +12,7 @@ for i in range(n):
     dist = abs(mid - i)
     start = dist
     end = n - dist
-    print(f"Row {i}: start={start}, end={end}")
+    print(f"Row {i}: start={start}, end={end}")  
     for j in range(start, end):
         print(f"i: {i}, j: {j}, each: {a[i][j]}")
         total += a[i][j]
